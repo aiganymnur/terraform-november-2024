c77328695aa8fa39a15d8ef09f5d607c9e29a5b3
aiganymnur <aiganymnurgazina@gmail.com>
2025-01-22 06:59:26 +0000

    added class 3 files

---
+++ class3/main.tf
@@ -0,0 +1,47 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+    depends_on = [aws_key_pair.deployer]
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  # availability_zone = "us-east-2c"
+  subnet_id = "subnet-0f7cd86f4e1a9ea82"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output ec2 {
+    value = aws_instance.web.public_ip
+    sensitive = true
+}
+
+# alias ta='terraform apply --auto-approve'
+# alias td='terraform destroy --auto-approve'
