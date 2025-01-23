45af7bd30d4024e6df6d7efe1e687e78ab6040f1
aiganymnur <aiganymnurgazina@gmail.com>
2025-01-23 03:05:42 +0000

    changed files

--- class5-backend/backend.tf
+++ class5-backend/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "kaizen-aiganymn"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "lock-state"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "kaizen-aiganymn"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "lock-state"
+#   }
+# }
 
